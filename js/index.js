56e6930fd5b999c21707b37a6a2ce9b62ca9a315
Jack <jtdruim@hotmail.co.uk>
2020-11-22 18:16:51 +0000

    need to add logic for gamewin

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ var newGameSound = new Audio(("./assets/NewGame.wav"))
 newGame.addEventListener("click", () => {
 
     //remove the button & play sound
-    newGame.style.display=("none");
+    newGame.style.display = ("none");
     newGameSound.play();
-    //call function to show board
-    gameBoard.showBoard();
-    gameBoard.scores();
+
+    //call startGame function
+    startGame();
 
 })
 
@@ -18,16 +18,28 @@ const gameBoard = (() => {
     //initialize the board with 9 blank cells
     const game = ["", "", "", "", "", "", "", "", ""];
     //
-    const showBoard = () => {
+    const showBoard = (marker) => {
         var container = document.querySelector(".container");
         var board = document.createElement("div");
         board.classList.add("board");
 
         //loop to go through and display 9 cells
-        for (var i=0; i < gameBoard.game.length; i++){
+        for (var i = 0; i < gameBoard.game.length; i++) {
             var cell = document.createElement("cell");
-            cell.classList.add("cell")
-            board.appendChild(cell);  
+
+            if (game[i] === "X"){
+                cell.innerText = player1.marker;
+                cell.classList.add("cellX");
+            } else if (game[i] === "O"){
+                cell.innerText = player2.marker;
+                cell.classList.add("cellO");
+            } else cell.classList.add("cell");
+
+
+            if (marker === "X"){
+                cell.setAttribute('data-after', `${player1.marker}`)
+            } else cell.setAttribute('data-after', `${player2.marker}`)
+            board.appendChild(cell);
         }
         container.appendChild(board);
 
@@ -35,14 +47,21 @@ const gameBoard = (() => {
         var cells = document.querySelectorAll("cell");
         cells.forEach((element, index) => {
             element.addEventListener('click', () => {
-
+                container.innerHTML = '';
+                game[index] = `${marker}`
+                checkWin();
+                startGame();
             })
         });
+        scores();
     };
 
     //set up and display the scores for each player
     const scores = () => {
-        var gameInfo = document.querySelector(".gameInfo");
+        var container = document.querySelector(".container");
+        var gameInfo = document.createElement("div");
+        gameInfo.classList.add('gameInfo');
+        container.appendChild(gameInfo);
         var Player_1 = document.createElement('h5');
         var Tied = document.createElement('h5');
         var Player_2 = document.createElement('h5');
@@ -66,6 +85,22 @@ const gameBoard = (() => {
         gameInfo.appendChild(score3);
     };
 
+    //checks if there's a win on the board
+    const checkWin = () => {
+        var winConditions =[
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        
+    }
+
     //reset the board back to its original state
     const reset = () => {
         gameBoard.game = ["", "", "", "", "", "", "", "", ""];
@@ -82,11 +117,21 @@ const gameBoard = (() => {
 
 
 //PLayer object
-function Player (name, marker, score){
+function Player(name, marker, score) {
     this.name = name;
     this.marker = marker;
     this.score = score;
 }
 
-const player1 = new Player ("PLAYER 1", "X", 0);
-const player2 = new Player ("COMPUTER", "O", 0);
+const player1 = new Player("PLAYER 1", "X", 0);
+const player2 = new Player("COMPUTER", "O", 0);
+var moves = 0;
+
+function startGame() {
+    var gameOver = false;
+    moves++;
+    console.log(moves);
+    if (moves%2 === 1){
+        gameBoard.showBoard(player1.marker)
+    } else gameBoard.showBoard(player2.marker)
+}
